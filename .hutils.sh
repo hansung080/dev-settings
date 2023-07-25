60fb3a10f3714e4be506f2d06e96b6c2861dff5e
hansung <hansung080@hanmail.net>
2023-07-25 20:50:11 +0900

    added type to make_new and added make_update

--- .hutils.sh
+++ .hutils.sh
@@ -1,3 +1,7 @@
+S_RESET='\033[0m'
+S_RED_BOLD='\033[1;31m'
+LOG_ERROR="${S_RED_BOLD}error${S_RESET}"
+
 SHELL_NAME="$(ps -p $$ | tail -1 | awk '{ print $4 }')"
 if [[ "${SHELL_NAME:0:1}" == "-" ]]; then
   SHELL_NAME="${SHELL_NAME:1}"
@@ -88,17 +92,23 @@ print_path() {
   done
 }
 
-make_new() {
-  if [ $# -ne 1 ]; then
-    echo "Usage) make_new <project>"
+make_check_args() {
+  local project="$1"
+  local type="$2"
+  local usage="usage: $3 <project> [bin | lib]"
+
+  if [[ "$project" == "" ]]; then
+    echo -e "$LOG_ERROR: <project> not provided"
+    echo "$usage"
     return 1
   fi
 
-  local project="$1"
-  mkdir -p "$project/src" "$project/test"
-  curl -fsSL 'https://raw.githubusercontent.com/hansung080/study/master/c/examples/make-sample/Makefile' | sed "s/make-sample/$project/g" > "$project/Makefile"
-  print_c_main "$project" > "$project/src/main.c"
-  print_c_main "$project-test" > "$project/test/main.c"
+  if [[ "$type" != "bin" ]] && [[ "$type" != "lib" ]]; then
+    echo -e "$LOG_ERROR: <type> must be either bin or lib"
+    echo "$usage"
+    return 1
+  fi
+  retrn 0
 }
 
 print_c_main() {
@@ -110,6 +120,34 @@ int main(int argc, char* argv[]) {
     return 0;
 }"
   echo "$content"
+}
+
+make_new() {
+  local project="$1"
+  local type="$2"
+  if ! make_check_args "$project" "$type" "make_new"; then
+    return 1
+  fi
+
+  mkdir -p "$project/src" "$project/test"
+  local makefile="$type.mk"
+  curl -fsSL "https://raw.githubusercontent.com/hansung080/study/master/c/examples/make-sample/$makefile" | sed "s/make-sample/$project/g" > "$project/$makefile"
+  curl -fsSL 'https://raw.githubusercontent.com/hansung080/study/master/c/examples/make-sample/project.mk' | sed "s/bin\.mk/$makefile/g" > "$project/Makefile"
+  print_c_main "$project-test" > "$project/test/main.c"
+  if [[ "$type" == "bin" ]]; then
+    print_c_main "$project" > "$project/src/main.c"
+  fi
+}
+
+make_update() {
+  local project="$1"
+  local type="$2"
+  if ! make_check_args "$project" "$type" "make_update"; then
+    return 1
+  fi
+
+  local makefile="$type.mk"
+  curl -fsSL "https://raw.githubusercontent.com/hansung080/study/master/c/examples/make-sample/$makefile" | sed "s/make-sample/$project/g" > "$makefile"
 }
 
 alias makev='make __verbose=true'
--- README.md
+++ README.md
@@ -3,14 +3,14 @@ Various things and stuff.
 
 ## Vim
 ### Download .vimrc
+To download `.vimrc` at the home directory, run:
 ```sh
-$ cd ~
-$ curl -fsSL 'https://raw.githubusercontent.com/hansung080/assets/master/.vimrc' -o '.vimrc'
+curl -fsSL 'https://raw.githubusercontent.com/hansung080/assets/master/.vimrc' -o '~/.vimrc'
 ```
 
 ## Shell
 ### Download .hutils.sh
+To download `.hutils.sh` at the home directory, run:
 ```sh
-$ cd ~
-$ curl -fsSL 'https://raw.githubusercontent.com/hansung080/assets/master/.hutils.sh' -o '.hutils.sh'
+$ curl -fsSL 'https://raw.githubusercontent.com/hansung080/assets/master/.hutils.sh' -o '~/.hutils.sh'
 ```
